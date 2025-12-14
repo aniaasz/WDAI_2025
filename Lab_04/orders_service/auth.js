fe0d27eb9b8f61939fe63e1d4965b63e3a83a62d
Anna Sznajder <amsznajder@gmail.com>
2025-12-14 22:48:25 +0100

    Lab4

---
+++ Lab_04/orders_service/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+const auth = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ error: 'No token provided' });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token, 'key');
+        req.user = decoded;
+        next();
+    } catch (err) {
+        return res.status(401).json({ error: 'Wrong token' });
+    }
+};
+
+module.exports = auth;
